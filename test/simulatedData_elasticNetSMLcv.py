32e359368af56163b49fd9234cef603062707152
Anhui Huang <anhuihng@gmail.com>
2023-05-11 22:36:05 -0400

    add local to remote

---
+++ test/simulatedData_elasticNetSMLcv.py
@@ -0,0 +1,42 @@
+"""
+Created on Fri April 21, 2023
+@author: Anhui Huang
+"""
+
+##
+import numpy as np
+from sparseSEM import elasticNetSML, elasticNetSMLcv, elasticNetSMLpoint
+
+##
+baseDataDir = './data/'
+B = np.loadtxt(baseDataDir + 'B.csv', dtype = 'float64', delimiter = ',', skiprows=1, usecols=range(1,31))
+X = np.loadtxt(baseDataDir + 'X.csv', dtype = 'float64', delimiter = ',', skiprows=1, usecols=range(1,201))
+Y = np.loadtxt(baseDataDir + 'Y.csv', dtype = 'float64', delimiter = ',', skiprows=1, usecols=range(1,201))
+M = np.loadtxt(baseDataDir + 'Missing.csv', dtype = 'float64', delimiter = ',', skiprows=1, usecols=range(1,201))
+
+##
+np.random.seed(0)
+result = elasticNetSMLcv(X, Y, M, B, verbose = 1);
+print(result.keys() )
+print(result['statistics'])
+
+##
+# visualize the inferred network:
+import networkx as nx
+import matplotlib.pyplot as plt
+
+adjacent = result['weight']
+r,c = adjacent.shape
+G = nx.Graph()
+for i in range(r):
+ for j in range( c):
+   if adjacent[i][j] != 0:
+      G.add_edge(i,j)
+
+nx.draw( G,with_labels=True)
+plt.show()
+
+
+
+
+
